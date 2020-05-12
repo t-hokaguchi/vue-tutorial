eca241905b9117b5e4647c66257586c0a661fe53
t-hokaguchi <jojo.gold.experience.1210@gmail.com>
2020-05-13 03:27:27 +0900

    Make Second

--- First/HandsOn/test01.js
+++ First/HandsOn/test01.js
@@ -1,5 +1,26 @@
-var msg = new Vue({});
+var msg = new Vue({
+  el: '#msg',
+  data: {
+    isCapital: true,
+  },
+});
 
-var product = new Vue({});
+var product = new Vue({
+  el: '#product',
+  data: {
+    items: [{ name: 'りんご' }, { name: 'ごりら' }, { name: 'ラッパー' }],
+  },
+});
 
-var app = new Vue({});
+var app = new Vue({
+  el: '#app',
+  data: {
+    day: 10,
+    msg: 'あけおめ',
+  },
+  methods: {
+    onDeleteDay: function () {
+      this.day--;
+    },
+  },
+});
