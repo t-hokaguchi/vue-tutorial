68dc22d25150e4803bc2ae691a5ee32dac1324f7
Kenta Hirashima <kenta.snickers@gmail.com>
2020-05-17 03:20:24 +0900

    第一回終了コミット

--- First/HandsOn/test01.js
+++ First/HandsOn/test01.js
@@ -1,5 +1,27 @@
-var msg = new Vue({});
+var msg = new Vue({
+    el: '#msg',
+    data: {
+        isCapital: true
+    }
+});
 
-var product = new Vue({});
+var product = new Vue({
+    el: '#product',
+    data: {
+        items: [
+            {name: 'りんご'}, {name: 'ゴリラ'}, {name: 'ラッパ'}
+        ]
+    }
+});
 
-var app = new Vue({});
+var app = new Vue({
+    el: '#app',
+    data: {
+        day:10
+    },
+    methods: {
+        onDeleteDay: function() {
+            this.day -= 1
+        }
+    }
+});
