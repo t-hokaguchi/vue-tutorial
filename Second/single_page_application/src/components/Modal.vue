e446a32d0e5dff9d8cc2ad5f107eeef555ea97a0
Kenta Hirashima <kenta.snickers@gmail.com>
2020-05-17 16:48:50 +0900

    モーダル実装

---
+++ Second/single_page_application/src/components/Modal.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="overlay">
+    <div id="content">
+      <p>これがモーダルウィンドウです。</p>
+      <button v-on:click="closeModal">Close</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    closeModal () {
+      this.$emit('from-child')
+    }
+  }
+
+}
+</script>
+
+<style>
+#overlay{
+  /* 要素を重ねた時の順番 */
+  z-index:1;
+
+  /* 画面全体を覆う設定 */
+  position:fixed;
+  top:0;
+  left:0;
+  width:100%;
+  height:100%;
+  background-color:rgba(0,0,0,0.5);
+
+  /* 画面の中央に要素を表示させる設定 */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+}
+#content{
+  z-index:2;
+  width:50%;
+  padding: 1em;
+  background:#fff;
+}
+</style>
--- Second/single_page_application/src/components/Welcome.vue
+++ Second/single_page_application/src/components/Welcome.vue
@@ -1,6 +1,37 @@
 <template>
   <div>
     <p>Welcome to Vue SPA!</p>
-    <router-link to="/">Go back Home</router-link>
+    <router-link to="/">Go back Home</router-link><br>
+    <button @click="openModal">Open modal</button>
+    <Modal v-show="showModal" v-on:from-child="closeModal" />
+    <!-- <div id="overlay" v-show="showModal">
+      <div id="content">
+        <p>これがモーダルウィンドウです。</p>
+        <button v-on:click="closeModal">Close</button>
+      </div>
+    </div> -->
   </div>
 </template>
+
+<script>
+import Modal from './Modal'
+export default {
+  name: 'Welcome',
+  components: { Modal },
+  data () {
+    return {
+      showModal: false
+    }
+  },
+
+  methods: {
+    openModal () {
+      this.showModal = true
+    },
+    closeModal () {
+      this.showModal = false
+    }
+  }
+
+}
+</script>
